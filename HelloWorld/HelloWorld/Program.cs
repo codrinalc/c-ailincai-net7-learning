8a831dec039dd870986f4a2eb718ce1e3b1b58f5
aCodrin-net7-wantsome <117767288+codrinalc@users.noreply.github.com>
2022-12-06 18:07:21 +0200

    test

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿using HelloWorld;
+Console.WriteLine("Exercitii!");
+var recap = new recap();
+recap.Go();
+
+
---
+++ HelloWorld/HelloWorld/recap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HelloWorld
+{
+    internal class recap
+    {
+        public void Go()
+
+        {
+
+
+
+
+
+
+
+        }
+
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+    
+
+
